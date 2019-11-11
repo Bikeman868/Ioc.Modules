72c85244768f598d6d7e76be66bdacf67a049ed7
Martin Halliday <martin.bikeman@gmail.com>
2019-11-11 00:13:37 -0800

    Reinstated the nuspec files so that inter-project dependencies work

---
+++ NuGet/Package.cmd
@@ -0,0 +1,5 @@
+nuget.exe pack Ioc.Modules.Contract.nuspec
+nuget.exe pack Ioc.Modules.nuspec
+nuget.exe pack Ioc.Modules.Ninject.nuspec
+nuget.exe pack Ioc.Modules.Unity.nuspec
+nuget.exe pack Ioc.Modules.Autofac.nuspec
